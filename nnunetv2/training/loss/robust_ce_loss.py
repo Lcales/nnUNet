4cc92575a5dc22d888bf1c1ce9d44e1c5861f407
Lcales <115400656+Lcales@users.noreply.github.com>
2025-05-09 17:54:47 +0200

    Update robust_ce_loss.py
    Aggiunta focal loss

--- nnunetv2/training/loss/robust_ce_loss.py
+++ nnunetv2/training/loss/robust_ce_loss.py
@@ -1,6 +1,7 @@
 import torch
 from torch import nn, Tensor
 import numpy as np
+import torch.nn.functional as F
 
 
 class RobustCrossEntropyLoss(nn.CrossEntropyLoss):
@@ -30,3 +31,25 @@ class TopKLoss(RobustCrossEntropyLoss):
         num_voxels = np.prod(res.shape, dtype=np.int64)
         res, _ = torch.topk(res.view((-1, )), int(num_voxels * self.k / 100), sorted=False)
         return res.mean()
+
+class FocalLossWithLogits(nn.Module):
+    def __init__(self, gamma=2.0, alpha=0.25, reduction='mean'):
+        super(FocalLossWithLogits, self).__init__()
+        self.gamma = gamma
+        self.alpha = alpha
+        self.reduction = reduction
+
+    def forward(self, logits, targets):
+        probs = torch.sigmoid(logits)
+        ce_loss = F.binary_cross_entropy_with_logits(logits, targets, reduction='none')
+
+        pt = torch.where(targets == 1, probs, 1 - probs)
+        focal_weight = self.alpha * (1 - pt) ** self.gamma
+        loss = focal_weight * ce_loss
+
+        if self.reduction == 'mean':
+            return loss.mean()
+        elif self.reduction == 'sum':
+            return loss.sum()
+        return loss
+
